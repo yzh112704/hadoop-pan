ee46ac9af2f41d76a1426e546af2fc1a87f79edd
yzh112704 <50942145+yzh112704@users.noreply.github.com>
2021-04-21 17:48:49 +0800

    change application name

--- src/main/java/com/pan/hadoop/HadoopPanApplication.java
+++ src/main/java/com/pan/hadoop/HadoopPanApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application {
+public class HadoopPanApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo1Application.class, args);
+        SpringApplication.run(HadoopPanApplication.class, args);
     }
 
 }
